9c1dea1c766bee1b7e58e3ebf6fda19534935524
JASON CHESNEY <waterson.park95@gmail.com>
2017-09-19 19:33:03 +0530

    My new Change

--- app/src/main/java/com/example/jasonchesney/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jasonchesney/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Code goes here... new new
+        //Code goes here... new new show
     }
 }
